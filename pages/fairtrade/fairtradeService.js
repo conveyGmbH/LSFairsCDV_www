a72012482a5c43b7c857e66ec8caea72066cb930
conveydev <gilbert.schwaab@outlook.de>
2017-11-24 06:10:19 +0100

    fairtrade implemented

---
+++ pages/fairtrade/fairtradeService.js
@@ -0,0 +1,98 @@
+﻿// service for page: event
+/// <reference path="~/www/lib/convey/scripts/strings.js" />
+/// <reference path="~/www/lib/convey/scripts/logging.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("Fairtrade", {
+        _initView: {
+            get: function () {
+                return AppData.getLgntInit("LGNTINITFairTrade");
+            }
+        },
+        _formatView: {
+            get: function () {
+                return AppData.getFormatView("LGNTINITFairTrade", 20517);
+            }
+        }
+    });
+    WinJS.Namespace.define("Fairtrade", {
+        initView: {
+            clear: function() {
+                Fairtrade._initView.clear();
+            },
+            select: function (complete, error, restriction) {
+                var ret;
+                Log.call(Log.l.trace, "Fairtrade.initView.");
+                if (typeof restriction === "number") {
+                    Log.print(Log.l.trace, "calling selectById... recordId=" + restriction);
+                    ret = Fairtrade._formatView.selectById(complete, error, restriction);
+                } else {
+                    // add language restriction in format view!
+                    if (!restriction) {
+                        restriction = {};
+                    }
+                    restriction.LanguageSpecID = AppData.getLanguageId();
+                    Log.print(Log.l.trace, "calling select... LanguageSpecID=" + restriction.LanguageSpecID);
+                    ret = Fairtrade._formatView.select(complete, error, restriction, {
+                        ordered: true,
+                        orderAttribute: "TITLE"
+                    });
+                }
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            deleteRecord: function (complete, error, recordId) {
+                Log.call(Log.l.trace, "Fairtrade.initView.");
+                var ret = Fairtrade._initView.deleteRecord(function () {
+                    if (typeof complete === "function") {
+                        complete();
+                    }
+                }, error, recordId);
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            update: function (complete, error, recordId, viewResponse) {
+                Log.call(Log.l.trace, "Fairtrade.initView.");
+                var ret = Fairtrade._initView.update(function () {
+                    if (typeof complete === "function") {
+                        complete();
+                    }
+                }, error, recordId, viewResponse);
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            insert: function (complete, error) {
+                Log.call(Log.l.trace, "Fairtrade.initView.");
+                var ret = Fairtrade._initView.insert(function () {
+                    if (typeof complete === "function") {
+                        complete();
+                    }
+                }, error, {
+                    FragengruppeID: 0
+                });
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            getNextUrl: function (response) {
+                Log.call(Log.l.trace, "Fairtrade.initView.");
+                var ret = Fairtrade._formatView.getNextUrl(response);
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            selectNext: function (complete, error, response, nextUrl) {
+                Log.call(Log.l.trace, "Fairtrade.initView.");
+                var ret = Fairtrade._formatView.selectNext(complete, error, response, nextUrl);
+                // this will return a promise to controller
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            relationName: Fairtrade._initView.relationName,
+            getRecordId: Fairtrade._initView.getRecordId
+        }
+    });
+})();
+
+
